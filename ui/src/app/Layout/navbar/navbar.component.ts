cc41e84e099cf470091ef7c98dbfedd0412c59b9
Anurag <ganurag624outlook@gmail.com>
2025-03-29 12:44:33 +0530

    minor fix

--- ui/src/app/Layout/navbar/navbar.component.ts
+++ ui/src/app/Layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,5 +9,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  private elRef = inject(ElementRef);
 
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    const navbar = this.elRef.nativeElement.querySelector('.navbar-collapse');
+    const button = this.elRef.nativeElement.querySelector('.navbar-toggler');
+
+    // Check if the click is outside the navbar and the toggle button
+    if (navbar && !this.elRef.nativeElement.contains(event.target) && !button.contains(event.target)) {
+      navbar.classList.remove('show');
+    }
+  }
 }
