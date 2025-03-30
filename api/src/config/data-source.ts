2c5abadcb0c8312463a7e207992bb9fccd7cb341
Anurag Gupta <ganurag624outlook@gmail.com>
2025-03-30 14:25:06 +0530

    a

--- api/src/config/data-source.ts
+++ api/src/config/data-source.ts
@@ -6,16 +6,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-
-    type: "postgres",  // defining  RDBMS
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,  // Automatically creates tables based on entities
-    logging: true,
-    entities: [Product, ProductImage], 
-    migrations: [],
-    subscribers: [],
+  type: "postgres", // defining  RDBMS
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true, // Automatically creates tables based on entities
+  logging: true,
+  ssl: {
+    rejectUnauthorized: false, // Allowing self-signed certificates
+  },
+  entities: [Product, ProductImage],
+  migrations: [],
+  subscribers: [],
 });
